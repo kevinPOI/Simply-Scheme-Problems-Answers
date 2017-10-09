9f8d9e01bddfc2e84c3b8f2cbb47a10da462e311
Kevin&poi~ <1076535188@qq.com>
2017-10-09 10:41:46 -0700

    basic functions library
    basic functions library

---
+++ Common Functions.rkt
@@ -0,0 +1,18 @@
+#|【Scheme Basic Functions by Kevin&poi~】|#
+
+;function: ture if input is a vowel, false if not.
+;argument: 1 character
+(define (vowel? char)
+  (member? char '(a e i o u)))
+;square the number
+;argument: 1 number
+(define (square num)
+  (* num num))
+;cube the number
+;argument: 1 number
+(define (cube num)
+  (* num num num))
+;find the second word/character in a string/word
+;argument: word/string
+(define (second x)
+  (first (bf x)))
